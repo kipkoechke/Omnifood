1e30a098e3824aad37d4bf8f056b96fe5c7dcb14
kipkoechke <chepkwonyke1@gmail.com>
2023-11-08 03:42:32 +0300

    Disabled default html scroll behavior

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+/////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const alllinks = document.querySelectorAll("a:link");
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // Disable default html scroll behavior
+    e.preventDefault();
+  });
+});
